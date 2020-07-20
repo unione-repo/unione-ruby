c485b6c99314f9b5a89927232ca6763e76655dda
Evgeniy Belyaev <eb.gh.cr@gmail.com>
2020-07-20 14:30:50 +0400

    Content was misplaced between files

--- lib/unione/helpers/webhook/webhook.rb
+++ lib/unione/helpers/webhook/webhook.rb
@@ -1,31 +1,16 @@
 module UniOne
-  class Template
-    attr_accessor :name, :subject, :template_engine, :global_substitutions, :from,
-                  :headers, :body, :attachments, :inline_attachments, :options
-
-    def initialize
-      @global_substitutions = {}
-      @from = {}
-      @attachments = []
-      @inline_attachments = []
-    end
+  class Webhook
+    attr_accessor :url, :event_format, :delivery_info, :single_event, :max_parallel, :events
 
     def to_json(*)
       {
-        template: {
-          id: '',
-          name: self.name,
-          subject: self.subject,
-          template_engine: self.template_engine,
-          global_substitutions: self.global_substitutions,
-          headers: self.headers,
-          body: self.body,
-          attachments: self.attachments,
-          inline_attachments: self.inline_attachments,
-          options: self.options
-        }.merge(self.from)
-          .delete_if { |_, value| value.to_s.strip == '' || value == [] || value == {}}
-      }
+        url: self.url,
+        event_format: self.event_format,
+        delivery_info: self.delivery_info,
+        single_event: self.single_event,
+        max_parallel: self.max_parallel,
+        events: self.events
+      }.delete_if { |_, value| value.to_s.strip == '' || value == [] || value == {}}
     end
   end
 end
