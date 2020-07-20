5408a38e12afbb2e1f26d00302b4e21bf5797309
Evgeniy Belyaev <eb.gh.cr@gmail.com>
2020-07-20 14:24:07 +0400

    Improve documentation

--- examples/webhook.rb
+++ examples/webhook.rb
@@ -1,39 +1,35 @@
 require_relative '../../../lib/unione-ruby'
 require 'json'
 
-def set_webhook
-  webhook = UniOne::Webhook.new
-  webhook.url = "http://example.com"
-  webhook.event_format = "json_post"
-  webhook.delivery_info = 1
-  webhook.single_event = 0
-  webhook.max_parallel = 10
-  webhook.events = {email_status: [
-    "sent",
-    "delivered",
-    "opened",
-    "hard_bounced",
-    "soft_bounced",
-    "spam",
-    "clicked",
-    "unsubscribed"
-  ], spam_block: ["*"]}
+# Set webhook
+webhook = UniOne::Webhook.new
+webhook.url = "http://example.com"
+webhook.event_format = "json_post"
+webhook.delivery_info = 1
+webhook.single_event = 0
+webhook.max_parallel = 10
+webhook.events = {email_status: [
+  "sent",
+  "delivered",
+  "opened",
+  "hard_bounced",
+  "soft_bounced",
+  "spam",
+  "clicked",
+  "unsubscribed"
+], spam_block: ["*"]}
 
-  puts webhook.to_json
+puts webhook.to_json
 
-  unione = UniOne::Client.new(data_center: 'eu1', lang: 'en', api_key: ENV['UNIONE_API_KEY'])
-  response = unione.set_webhook(webhook.to_json)
-  puts response.status
-  puts response.body.to_h
-  puts response.headers
-end
+unione = UniOne::Client.new(data_center: 'eu1', lang: 'en', api_key: ENV['UNIONE_API_KEY'])
+response = unione.set_webhook(webhook.to_json)
+puts response.status
+puts response.body.to_h
+puts response.headers
 
 # Get webhook
 unione = UniOne::Client.new(data_center: 'eu1', lang: 'en', api_key: ENV['UNIONE_API_KEY'])
 response = unione.get_webhook("http://example.com")
-puts response.status
-puts response.body.to_h
-puts response.headers
 
 # Delete webhook
 response = unione.delete_webhook("http://example.com")
